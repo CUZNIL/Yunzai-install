e2f487aeff3aa81a4eb18d3e4aa1eb9d4dfc38a4
硫酸钡Barite <iamliujunxi@qq.com>
2023-04-23 06:16:15 +0000

    update download/TRSS_AllBot/Yunzai/plugins/example/定时重启.js.
    Signed-off-by: 硫酸钡Barite <iamliujunxi@qq.com>

--- download/TRSS_AllBot/Yunzai/plugins/example/定时重启.js
+++ download/TRSS_AllBot/Yunzai/plugins/example/定时重启.js
@@ -4,7 +4,9 @@ let ThisFilePath = "plugins/example/定时重启.js"
 
 
 let RestartQuest
-import { Restart } from '../other/restart.js'
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+const { exec } = require('child_process')
 import fs from 'node:fs'
 export class RestartAtTime extends plugin {
     constructor() {
@@ -18,11 +20,14 @@ export class RestartAtTime extends plugin {
                     fnc: 'set',
                     permission: 'master'
                 }
-            ]
+            ],
         })
         let RestartMs = ((Number(RestartTime.substring(0, RestartTime.lastIndexOf(":"))) - 8) * 60 + Number(RestartTime.substring(RestartTime.lastIndexOf(":") + 1, RestartTime.length))) * 60 * 1000
         let time = (RestartMs - new Date().getTime()) % 86400000 + 86400000
-        RestartQuest = setTimeout(() => this.restart(), time)
+        RestartQuest = setTimeout(() => {
+            logger.mark(`定时重启插件：当前时间已经到达${RestartTime}，即将重启。`)
+            this.restart()
+        }, time)
     }
     async set() {
         clearTimeout(RestartQuest)
@@ -33,7 +38,21 @@ export class RestartAtTime extends plugin {
             fs.writeFileSync(ThisFilePath, file.replace(RestartTime, change), 'utf-8')
         }, 3000)
     }
-    restart() {
-        new Restart(this.e).restart()
+    async restart() {
+        let cm = 'npm run restart'
+        let ret = await new Promise((resolve, reject) => {
+            exec('pnpm -v', { windowsHide: true }, (error, stdout, stderr) => {
+                resolve({ error, stdout, stderr })
+            })
+        })
+        if (ret.stdout) cm = 'pnpm run restart'
+        exec(cm, { windowsHide: true }, (error, stdout, stderr) => {
+            if (error) {
+                logger.error(`定时重启插件：重启失败${error.stack}`)
+            } else if (stdout) {
+                logger.mark('定时重启插件：重启成功，运行已由前台转为后台')
+                process.exit()
+            }
+        })
     }
 }
